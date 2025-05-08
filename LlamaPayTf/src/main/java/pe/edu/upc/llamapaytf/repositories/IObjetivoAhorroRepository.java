18dd35d6adaf75b3e80f06775f5e3d5c25cce325
Ivana Panduro Rodriguez <u20201c143@upc.edu.pe>
2025-05-08 12:37:31 -0500

    same

--- LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/repositories/IObjetivoAhorroRepository.java
+++ LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/repositories/IObjetivoAhorroRepository.java
@@ -11,16 +11,16 @@ import java.util.List;
 public interface IObjetivoAhorroRepository extends JpaRepository<ObjetivoAhorro,Integer> {
     List<ObjetivoAhorro> findByUser_IdUser(int userId);
 
-    @Query(value = "SELECT u.name_user AS nombreUsuario, SUM(o.monto_ahorrado) AS montoTotalAhorrado " +
-            "FROM objetivo_ahorro o " +
-            "INNER JOIN usuario u ON o.id_user = u.id_user " +
+    @Query(value = "SELECT u.name_user AS nombreUsuario, SUM(o.monto_actual) AS montoTotalAhorrado \n" +
+            "FROM objetivo_ahorro o\n" +
+            "INNER JOIN Users u ON o.usuario_id = u.id_user \n" +
             "GROUP BY u.name_user", nativeQuery = true)
     List<String[]> amountTotalByUser();
 
-    @Query(value = "SELECT TO_CHAR(o.fecha_creacion, 'Month') AS mes, COUNT(*) " +
-            "FROM objetivo_ahorro o " +
-            "GROUP BY mes " +
-            "ORDER BY MIN(o.fecha_creacion)", nativeQuery = true)
+    @Query(value = "SELECT TO_CHAR(o.fecha_inicio, 'Month') AS mes, COUNT(*) \n" +
+            "FROM objetivo_ahorro o \n" +
+            "GROUP BY mes \n" +
+            "ORDER BY MIN(o.fecha_inicio)", nativeQuery = true)
     List<String[]> contarObjetivosPorMes();
 
 }
