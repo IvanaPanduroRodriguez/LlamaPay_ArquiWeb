220aea64cac84a4946a160fcbd6339f8558b6058
jfcats <144728456+jfcats@users.noreply.github.com>
2025-05-09 16:10:45 -0500

    Update ITransaccionService.java

--- LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/servicesinterfaces/ITransaccionService.java
+++ LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/servicesinterfaces/ITransaccionService.java
@@ -6,12 +6,12 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface ITransaccionService {
+    List<Transaccion> list();
     void insert(Transaccion transaccion);
+    Transaccion listID(int id);
     void update(Transaccion transaccion);
     void delete(int id);
-    List<Transaccion> list();
-    Transaccion listID(int id);
-
     List<Transaccion> findByMontoMayorAndMes(BigDecimal monto, int mes);
     List<Transaccion> findByDescripcionAndMes(String descripcion, int mes);
+    List<Transaccion> findByDescripcion(String descripcion);
 }
