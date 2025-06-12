c7aaa13a370e6168f557430d4c7de6b23d7d2d8b
Ivana Panduro Rodriguez <u20201c143@upc.edu.pe>
2025-06-12 00:14:11 -0500

    intento de listar en el frontend

--- Llamapay_Frontend/src/app/components/categoria/categoria.ts
+++ Llamapay_Frontend/src/app/components/categoria/categoria.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Listarcategoria } from './listarcategoria/listarcategoria';
 
 @Component({
   selector: 'app-categoria',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    Listarcategoria
+  ],
   templateUrl: './categoria.html',
   styleUrl: './categoria.css'
 })
 export class Categoria {
-
+  constructor(public route:ActivatedRoute) { 
+  }
 }
