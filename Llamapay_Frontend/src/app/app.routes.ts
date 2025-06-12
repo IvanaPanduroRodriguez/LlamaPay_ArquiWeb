c457b999df8eae7ff06caf12b6b681bf1504b127
Ivana Panduro Rodriguez <u20201c143@upc.edu.pe>
2025-06-12 15:12:42 -0500

    registrar funcional con css

--- Llamapay_Frontend/src/app/app.routes.ts
+++ Llamapay_Frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Categoria } from './components/categoria/categoria';
+import { Insertareditar } from './components/categoria/insertareditar/insertareditar';
 
 export const routes: Routes = [
   {
     path:'categorias',component:Categoria,
-    //children:[{ path:'formulario',component:InsertareditarComponent}]
+    children:[
+      { 
+        path:'formulario',component:Insertareditar
+      }
+    ]
   }
 ];
