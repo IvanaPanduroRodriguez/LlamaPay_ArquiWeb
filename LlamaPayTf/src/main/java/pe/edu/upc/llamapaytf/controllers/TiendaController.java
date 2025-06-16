42b3105fc3cbc72e91807eecd4709991336cfbec
Ivana Panduro Rodriguez <u20201c143@upc.edu.pe>
2025-06-15 19:02:17 -0500

    seguridad liberada todas las tablas

--- LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/controllers/TiendaController.java
+++ LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/controllers/TiendaController.java
@@ -18,7 +18,7 @@ public class TiendaController {
     private ITiendaService tS;
 
     @GetMapping
-    @PreAuthorize("hasAnyAuthority('CLIENTE', 'ADMIN','FINANZAS','TESTER')")
+    //@PreAuthorize("hasAnyAuthority('CLIENTE', 'ADMIN','FINANZAS','TESTER')")
     public List<TiendaDTO> listar() {
         return tS.list().stream().map(x->{
             ModelMapper modelMapper = new ModelMapper();
@@ -27,7 +27,7 @@ public class TiendaController {
     }
 
     @PostMapping
-    @PreAuthorize("hasAuthority('ADMIN')")
+    //@PreAuthorize("hasAuthority('ADMIN')")
     public void insertar(@RequestBody TiendaDTO dto) {
         ModelMapper m = new ModelMapper();
         Tienda t = m.map(dto, Tienda.class);
@@ -35,20 +35,20 @@ public class TiendaController {
     }
 
     @PutMapping
-    @PreAuthorize("hasAuthority('ADMIN')")
+    //@PreAuthorize("hasAuthority('ADMIN')")
     public void modificar(@RequestBody TiendaDTO dto) {
         ModelMapper m = new ModelMapper();
         Tienda t = m.map(dto, Tienda.class);
         tS.update(t);
     }
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAuthority('ADMIN')")
+    //@PreAuthorize("hasAuthority('ADMIN')")
     public void eliminar(@PathVariable("id") int id) {
         tS.delete(id);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyAuthority('ADMIN','FINANZAS','TESTER')")
+    //@PreAuthorize("hasAnyAuthority('ADMIN','FINANZAS','TESTER')")
     public TiendaDTO buscarID(@PathVariable("id") int id) { //debe tener indicado que variable estara en la ruta
         ModelMapper m = new ModelMapper();
         TiendaDTO dto=m.map(tS.listarId(id),TiendaDTO.class);
