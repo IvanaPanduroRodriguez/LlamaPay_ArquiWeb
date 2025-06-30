a88c7bf92614784d9696d44fdaec747a4067876f
John William Machaca Saavedra <136102139+U202214514@users.noreply.github.com>
2025-06-30 00:36:52 -0500

    Intento de api pasarela de pagos y calendario

--- Llamapay_Frontend/src/app/components/metodopago/listarmetodopago/listarmetodopago.ts
+++ Llamapay_Frontend/src/app/components/metodopago/listarmetodopago/listarmetodopago.ts
@@ -19,7 +19,7 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class Listarmetodopago implements OnInit {
   dataSource: MatTableDataSource<MetodoPago> = new MatTableDataSource();
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4','c5','c6'];
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4','c5','c6','c7'];
   constructor(private mpS: MetodoPagoService) { }
 
 ngOnInit(): void {
@@ -31,7 +31,29 @@ ngOnInit(): void {
     })
 
   }
+simularPago(element: MetodoPago) {
+  alert(`Pago simulado exitosamente con el método: ${element.nombreMetodoPago}`);
+}
 
+agregarACalendario(element: MetodoPago) {
+  const fecha = new Date().toISOString();
+  const titulo = `Recordatorio - Pago con ${element.nombreMetodoPago}`;
+  const link = `data:text/calendar;charset=utf8,BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:${titulo}
+DTSTART:${fecha.replace(/[-:]/g, '').split('.')[0]}Z
+DTEND:${fecha.replace(/[-:]/g, '').split('.')[0]}Z
+DESCRIPTION:${element.descripcion}
+END:VEVENT
+END:VCALENDAR`;
+
+  const encodedUri = encodeURI(link);
+  const a = document.createElement('a');
+  a.href = encodedUri;
+  a.download = `${titulo}.ics`;
+  a.click();
+}
   eliminar(id: number) {
     this.mpS.deleteS(id).subscribe(data=>{
       this.mpS.list().subscribe(data=>{
--- Llamapay_Frontend/src/app/components/objetivo-ahorro/listarobjetivoahorro/listarobjetivoahorro.ts
+++ Llamapay_Frontend/src/app/components/objetivo-ahorro/listarobjetivoahorro/listarobjetivoahorro.ts
@@ -18,7 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class Listarobjetivoahorro implements OnInit {
   dataSource: MatTableDataSource<ObjetivoAhorro> = new MatTableDataSource()
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9']
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9','c10']
   constructor(private oS: ObjetivoAhorroService) { }
 
   ngOnInit(): void {
@@ -29,6 +29,29 @@ export class Listarobjetivoahorro implements OnInit {
       this.dataSource = new MatTableDataSource(data)
     })
   }
+
+  agregarACalendario(meta: ObjetivoAhorro) {
+  const fechaFinISO = new Date(meta.fechaFin).toISOString();
+  const titulo = `Meta de Ahorro: ${meta.nombreMeta}`;
+  const descripcion = `Objetivo de ahorro hasta ${meta.fechaFin} por un monto de S/. ${meta.montoMeta}`;
+
+  const eventoICS = `BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:${titulo}
+DESCRIPTION:${descripcion}
+DTSTART:${fechaFinISO.replace(/[-:]/g, '').split('.')[0]}Z
+DTEND:${fechaFinISO.replace(/[-:]/g, '').split('.')[0]}Z
+END:VEVENT
+END:VCALENDAR`;
+
+  const encodedUri = encodeURI("data:text/calendar;charset=utf8," + eventoICS);
+  const a = document.createElement('a');
+  a.href = encodedUri;
+  a.download = `${titulo}.ics`;
+  a.click();
+}
+
   eliminar(Objetivo_id: number) {
     this.oS.deleteS(Objetivo_id).subscribe(data=>{
       this.oS.list().subscribe(data=>{
