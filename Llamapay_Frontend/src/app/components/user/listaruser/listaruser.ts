e244965ab74fafe76488ad0ec618f8d5ca3596ab
John William Machaca Saavedra <136102139+U202214514@users.noreply.github.com>
2025-06-30 00:16:11 -0500

    CARDS PARA USER

--- Llamapay_Frontend/src/app/components/user/listaruser/listaruser.ts
+++ Llamapay_Frontend/src/app/components/user/listaruser/listaruser.ts
@@ -6,20 +6,33 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-listaruser',
   imports: [MatTableModule, CommonModule,
     MatButtonModule,
     RouterLink,
-    MatIconModule],
+    MatIconModule,
+  MatCardModule],
   templateUrl: './listaruser.html',
   styleUrl: './listaruser.css'
 })
 export class Listaruser implements OnInit {
   dataSource: MatTableDataSource<User> = new MatTableDataSource();
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4','c5','c6','c7'];
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4','c5','c6','c7', 'c8'];
   constructor(private uS: UserService) { }
+
+  detalleSeleccionado: User | null = null;
+
+
+  verDetalle(element: User) {
+    this.detalleSeleccionado = this.detalleSeleccionado === element ? null : element;
+  }
+
+  cerrarDetalle() {
+    this.detalleSeleccionado = null;
+  }
 
   ngOnInit(): void {
     this.uS.list().subscribe(data => {
